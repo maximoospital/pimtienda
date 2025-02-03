ec824192f3e42f450f0bd1fb456eff7b3cbe70a5
Maximo Ospital <59743027+maximoospital@users.noreply.github.com>
2025-02-03 19:33:30 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/pimtienda" : "",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/pimtienda/" : "",
+};
+
+module_exports = nextConfig;
